4a1feb51709dd4c4bacfcab711cd332b878cdeb1
Srchrystian1234 <95313820+Srchrystian1234@users.noreply.github.com>
2024-05-08 16:49:21 -0400

    commit

--- src/components/SrChrystian/teste.jsx
+++ src/components/SrChrystian/teste.jsx
@@ -2,10 +2,10 @@ import "../SrChrystian/teste.css"
 function Teste () {
     return ( 
     <div id="ola"> 
-        <br /><h1 id="texto"> hello word </h1><br />
-        <h2>bom dia</h2>
+        <br /><h1 className="igual"> hello word </h1><br />
+        <h2 className="igual" >bom dia</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, recusandae non! A veniam blanditiis eveniet eos tenetur nemo ipsam sapiente aperiam dolore nisi quia, itaque, ullam reprehenderit saepe id ipsa!</p>
-        
+        <p><button>botao</button></p>
     </div>
     
     )
